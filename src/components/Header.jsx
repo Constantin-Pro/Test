bdc48db351d3c46d45f05ebbc8f457a14cb3a57e
Constantin <Mail-For-App@ya.ru>
2023-02-04 22:44:06 +0300

    Initial commit from the WebStorm

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<header className='header'>
+	    <img src='https://avatars.mds.yandex.net/i?id=f85455c1ff55967b55eb9b05ba3a4ba003799867-4141662-images-thumbs&n=13'/>
+	  </header>
+	)
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+	return (
+		<div className='content'>
+		<div>
+			<img src='https://avatars.mds.yandex.net/get-autoru-vos/5013756/927ebafed2e2200f3806fa9725699ea6/1200x900'/>
+		</div>
+		<div>
+		Ava + description
+		</div>
+		<div>
+		  My posts
+			<div>
+			  New post
+			</div>
+			<div>			
+				<div>
+				  Post 1
+				</div>
+				<div>
+				  Post 2
+				</div>
+			</div>
+		</div>
+	  </div>
+	)
+
+}
+
+export default Profile;
